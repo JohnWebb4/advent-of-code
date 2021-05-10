4857d9ed2844d07a63144a999109945e9ab603ae
John Webb <john.webb@keap.com>
2021-05-10 16:33:10 -0700

    Complete 2015 day 4

---
+++ src/main/java/advent/year2015/day4/IdealStockingStuffer.java
@@ -0,0 +1,39 @@
+package advent.year2015.day4;
+
+// In: String secret
+// Out: Number to produce hash with # zeros
+// Side effects: None
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+// Brute force
+// Start at zero
+// Add to secret and get MD5 hash
+// Compare for correct number of starting zeros
+// // If correct return number
+// // Else increment and continue re-hashing
+public class IdealStockingStuffer {
+    public static long getLongToHashZeros(String secret, int numZeros) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            String hash = "";
+            long countToHashZeros = 0;
+            do {
+                md.update((secret + countToHashZeros).getBytes());
+                hash = DatatypeConverter.printHexBinary(md.digest()).toUpperCase();
+
+                if (hash.startsWith("0".repeat(numZeros))) {
+                    return countToHashZeros;
+                }
+
+                countToHashZeros++;
+            } while (true);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/advent/year2015/day4/IdealStockingStufferTest.java
@@ -0,0 +1,27 @@
+package advent.year2015.day4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IdealStockingStufferTest {
+    @Test
+    public void getDecimalToHashZeros_609043() {
+        assertEquals(609043, IdealStockingStuffer.getLongToHashZeros("abcdef", 5));
+    }
+
+    @Test
+    public void getDecimalToHashZeros_1048970() {
+        assertEquals(1048970, IdealStockingStuffer.getLongToHashZeros("pqrstuv", 5));
+    }
+
+    @Test
+    public void getDecimalToHashZeros_346386() {
+        assertEquals(346386, IdealStockingStuffer.getLongToHashZeros("iwrupvqb", 5));
+    }
+
+    @Test
+    public void getDecimalToHashZeros_9958218() {
+        assertEquals(9958218, IdealStockingStuffer.getLongToHashZeros("iwrupvqb", 6));
+    }
+}
