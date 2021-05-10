721b9edcd67911d99c0fc145bb3ad0af645745b2
John Webb <john.webb@keap.com>
2021-05-09 21:48:01 -0700

    Complete year 2015 day 2

---
+++ src/main/java/advent/year2015/day2/ToldNoMath.java
@@ -0,0 +1,46 @@
+package advent.year2015.day2;
+
+import java.util.Arrays;
+
+public class ToldNoMath {
+    public static double getRequiredWrappingArea(String dimensions) {
+        String[] dimensionArr = dimensions.split("\n");
+        double requiredArea = 0;
+
+        for (String dimensionStr : dimensionArr) {
+            double[] dimension = Arrays.stream(dimensionStr.split("x")).mapToDouble(Float::parseFloat).toArray();
+
+            double side1 = dimension[0] * dimension[1];
+            double side2 = dimension[0] * dimension[2];
+            double side3 = dimension[1] * dimension[2];
+
+            requiredArea +=  2 * side1 + 2 * side2 + 2 * side3;
+
+            if (side1 <= side2 && side1 <= side3) {
+                requiredArea += side1;
+            } else if (side2 <= side3) {
+                requiredArea += side2;
+            } else {
+                requiredArea += side3;
+            }
+        }
+
+        return requiredArea;
+    }
+
+    public static double getRequiredRibbonLength(String dimensions) {
+        String[] dimensionArr = dimensions.split("\n");
+        double requiredLength = 0;
+
+        for (String dimensionStr : dimensionArr) {
+            double[] dimension = Arrays.stream(dimensionStr.split("x")).mapToDouble(Float::parseFloat).toArray();
+
+            requiredLength += dimension[0] * dimension[1] * dimension[2];
+
+            double[] sides = Arrays.stream(dimension).sorted().toArray();
+            requiredLength += 2 * sides[0] + 2 * sides[1];
+        }
+
+        return requiredLength;
+    }
+}
---
+++ src/test/java/advent/year2015/day2/ToldNoMathTest.java
@@ -0,0 +1,53 @@
+package advent.year2015.day2;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+public class ToldNoMathTest {
+    public static String input;
+
+    @BeforeClass
+    public static void initialize() {
+        try {
+            input = new String(Files.readAllBytes(Paths.get("./src/test/java/advent/year2015/day2/input.txt")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getRequiredWrappingArea_58() {
+        assertEquals(58, ToldNoMath.getRequiredWrappingArea("2x3x4"), 0.01);
+    }
+
+    @Test
+    public void getRequiredWrappingArea_43() {
+        assertEquals(43, ToldNoMath.getRequiredWrappingArea("1x1x10"), 0.01);
+    }
+
+    @Test
+    public void getRequiredWrappingArea_0() {
+        assertEquals(1588178, ToldNoMath.getRequiredWrappingArea(input), 0.01);
+    }
+
+    @Test
+    public void getRequiredRibbonLength_34() {
+        assertEquals(34, ToldNoMath.getRequiredRibbonLength("2x3x4"), 0.01);
+    }
+
+    @Test
+    public void getRequiredRibbonLength_14() {
+        assertEquals(14, ToldNoMath.getRequiredRibbonLength("1x1x10"), 0.01);
+    }
+
+    @Test
+    public void getRequiredRibbonLength_0() {
+        assertEquals(3783758, ToldNoMath.getRequiredRibbonLength(input), 0.01);
+    }
+}
