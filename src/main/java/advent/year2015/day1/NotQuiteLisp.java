9e282af39411f330a7988da0f3e898e11f273a3d
John Webb <john.webb@keap.com>
2021-05-09 21:16:14 -0700

    Complete 2015 day 1

---
+++ src/main/java/advent/year2015/day1/NotQuiteLisp.java
@@ -0,0 +1,41 @@
+package advent.year2015.day1;
+
+public class NotQuiteLisp {
+    public static int getFloor(String instructions) {
+        int floor = 0;
+
+        if (instructions != null) {
+            for (char c : instructions.toCharArray()) {
+                if (c == '(') {
+                    floor++;
+                } else if (c == ')') {
+                    floor--;
+                }
+            }
+        }
+
+        return floor;
+    }
+
+    public static int getIndexFirstEnterBasement(String instructions) {
+        int floor = 0;
+
+        if (instructions != null) {
+            for (int i = 0; i < instructions.length(); i++) {
+                char c = instructions.charAt(i);
+
+                if (c == '(') {
+                    floor++;
+                } else if (c == ')') {
+                    floor--;
+                }
+
+                if (floor == -1) {
+                    return i + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ src/test/java/advent/year2015/day1/NotQuiteLispTest.java
@@ -0,0 +1,54 @@
+package advent.year2015.day1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class NotQuiteLispTest {
+    public static String input;
+
+    @BeforeClass
+    public static void initialize() {
+        try {
+            input = new String(Files.readAllBytes(Paths.get("./src/test/java/advent/year2015/day1/input.txt")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetFloor_0() {
+        assertEquals(0, NotQuiteLisp.getFloor("(())"));
+    }
+
+    @Test
+    public void testGetFloor_0_1() {
+        assertEquals(0, NotQuiteLisp.getFloor("()()"));
+    }
+
+    @Test
+    public void testGetFloor_74() {
+        assertEquals(74, NotQuiteLisp.getFloor(input));
+    }
+
+    @Test
+    public void testGetIndexFirstEnterBasement_1() {
+        assertEquals(1, NotQuiteLisp.getIndexFirstEnterBasement(")"));
+    }
+
+    @Test
+    public void testGetIndexFirstEnterBasement_5() {
+        assertEquals(5, NotQuiteLisp.getIndexFirstEnterBasement("()())"));
+    }
+
+    @Test
+    public void testGetIndexFirstEnterBasement_0() {
+        assertEquals(1795, NotQuiteLisp.getIndexFirstEnterBasement(input));
+    }
+}
