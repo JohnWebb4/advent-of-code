9604ba1924dc0421a67ab342a776dbaa61f90501
John Webb <john.webb94@gmail.com>
2021-11-18 11:03:22 -0700

    Complete 2016 day 14

--- src/main/java/advent/year2016/day14/OneTimePad.java
+++ src/main/java/advent/year2016/day14/OneTimePad.java
@@ -1,15 +1,14 @@
+/* Licensed under Apache-2.0 */
 package advent.year2016.day14;
 
-import javax.xml.bind.DatatypeConverter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import javax.xml.bind.DatatypeConverter;
 
 public class OneTimePad {
     public static final Pattern REPEATED_DIGIT_PATTERN = Pattern.compile(".*?((.)\\2{2,}).*");
@@ -29,12 +28,12 @@ public class OneTimePad {
             MessageDigest md = MessageDigest.getInstance("MD5");
 
             // While count is less than key number
-            for (long i = 0; numKeysFound < keyNumber && i < 30000; i++) {
+            for (long i = 0; numKeysFound < keyNumber && i < 40000; i++) {
                 String data = salt + i;
                 md.update(data.getBytes());
                 byte[] digest = md.digest();
                 String hash = DatatypeConverter
-                        .printHexBinary(digest).toUpperCase();
+                        .printHexBinary(digest).toLowerCase(Locale.ROOT);
 
                 Matcher repeatedDigitMatcher = REPEATED_DIGIT_PATTERN.matcher(hash);
                 if (repeatedDigitMatcher.find()) {
@@ -50,23 +49,74 @@ public class OneTimePad {
                             for (Long index : indexMatches) {
                                 numKeysFound++;
 
-                                System.out.println(String.format("YO %s %s %s %s", hash, index, i, numKeysFound));
-
                                 if (numKeysFound == keyNumber) {
                                     return index;
                                 }
                             }
 
                             digitPossibleIndexMatchesMap.put(digit, new LinkedList<>());
-                            break;
-                        case 3:
-                            digitPossibleIndexMatchesMap.putIfAbsent(digit, new LinkedList<>());
                             digitPossibleIndexMatchesMap.get(digit).add(i);
                             break;
                         default:
+                            if (repeatedString.length() >= 3) {
+                                digitPossibleIndexMatchesMap.putIfAbsent(digit, new LinkedList<>());
+                                digitPossibleIndexMatchesMap.get(digit).add(i);
+                            }
                             break;
                     }
 
+                }
+            }
+        } catch (NoSuchAlgorithmException e) {
+            // No op
+        }
+
+        return -1;
+    }
+
+    public static long getIndexOfNthKeyStretched(String salt, int keyNumber, int numRepetativeHash) {
+        // Map of digit to list of three matches
+        Map<Character, List<Long>> digitPossibleIndexMatchesMap = new HashMap<>();
+        // long index of last match
+        int numKeysFound = 0;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // While count is less than key number
+            for (long i = 0; numKeysFound < keyNumber && i < 40000; i++) {
+                String hash = salt + i;
+
+                for (int indexHashes = 0; indexHashes <= numRepetativeHash; indexHashes++) {
+                    md.update(hash.getBytes());
+                    byte[] digest = md.digest();
+                    hash = DatatypeConverter
+                            .printHexBinary(digest).toLowerCase(Locale.ROOT);
+                }
+
+                Matcher repeatedDigitMatcher = REPEATED_DIGIT_PATTERN.matcher(hash);
+                if (repeatedDigitMatcher.find()) {
+                    String repeatedString = repeatedDigitMatcher.group(1);
+                    char digit = repeatedString.charAt(0);
+
+                    if (repeatedString.length() >= 5) {
+                        List<Long> indexMatches = digitPossibleIndexMatchesMap.getOrDefault(digit, new LinkedList<>());
+                        long finalI = i;
+                        indexMatches = indexMatches.stream().filter(index -> index >= finalI - 999).collect(Collectors.toList());
+
+                        for (Long index : indexMatches) {
+                            numKeysFound++;
+
+                            if (numKeysFound == keyNumber) {
+                                return index;
+                            }
+                        }
+
+                        digitPossibleIndexMatchesMap.put(digit, new LinkedList<>());
+                        digitPossibleIndexMatchesMap.get(digit).add(i);
+                    } else if (repeatedString.length() >= 3) {
+                        digitPossibleIndexMatchesMap.putIfAbsent(digit, new LinkedList<>());
+                        digitPossibleIndexMatchesMap.get(digit).add(i);
+                    }
                 }
             }
         } catch (NoSuchAlgorithmException e) {
