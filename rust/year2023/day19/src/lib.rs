b86e9f1c688597815ccdbf766a7bac87b6064931
John Webb <john.webb94@gmail.com>
2024-01-07 20:23:29 -0700

    Setup 2023 day 19

---
+++ rust/year2023/day19/src/lib.rs
@@ -0,0 +1,33 @@
+pub fn get_sum_accepted_ratings(input: &str) -> i32 {
+    println!("Hello, world!");
+
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use crate::get_sum_accepted_ratings;
+
+    const TEST_INPUT_1: &str = "px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}";
+
+    #[test]
+    fn test_get_sum_accepted_ratings() {
+        assert_eq!(19114, get_sum_accepted_ratings(TEST_INPUT_1));
+    }
+}
