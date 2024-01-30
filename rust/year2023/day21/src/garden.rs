223e51b774d7045de3cbc85120af05d6072aa985
John Webb <john.webb94@gmail.com>
2024-01-30 11:25:28 -0700

    Get part one test

--- rust/year2023/day21/src/garden.rs
+++ rust/year2023/day21/src/garden.rs
@@ -1,18 +1,47 @@
 use std::collections::HashMap;
 
+#[derive(Debug, PartialEq)]
 pub enum Cell {
     Garden,
     Start,
     Rock,
 }
 
+impl Cell {
+    pub fn from_char(c: char) -> Result<Cell, ()> {
+        match c {
+            '.' => Ok(Cell::Garden),
+            '#' => Ok(Cell::Rock),
+            'S' => Ok(Cell::Start),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Garden {
+    pub start_x: usize,
+    pub start_y: usize,
+
+    pub height: usize,
+    pub width: usize,
+
     inner_map: HashMap<String, Cell>,
 }
 
-pub fn new_garden() -> Garden {
+pub fn new_garden(
+    inner_map: HashMap<String, Cell>,
+    start_x: usize,
+    start_y: usize,
+    width: usize,
+    height: usize,
+) -> Garden {
     Garden {
-        inner_map: HashMap::new(),
+        inner_map,
+        start_x,
+        start_y,
+        width,
+        height,
     }
 }
 
@@ -24,4 +53,21 @@ impl Garden {
     pub fn get_cell(&self, x: usize, y: usize) -> Option<&Cell> {
         self.inner_map.get(&Garden::get_key(x, y))
     }
+
+    // pub fn print_debug(&self) {
+    //     for y in 0..self.height {
+    //         for x in 0..self.width {
+    //             let cell = self.get_cell(x, y).unwrap();
+
+    //             match cell {
+    //                 Cell::Garden => print!("."),
+    //                 Cell::Rock => print!("#"),
+    //                 Cell::Start => print!("S"),
+    //                 _ => {}
+    //             }
+    //         }
+
+    //         println!()
+    //     }
+    // }
 }
--- rust/year2023/day21/src/lib.rs
+++ rust/year2023/day21/src/lib.rs
@@ -1,14 +1,88 @@
-use garden::{new_garden, Garden};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet, LinkedList},
+};
+
+use garden::{new_garden, Cell, Garden};
 
 mod garden;
 
-pub fn get_garden_plots_in_steps(num_steps: usize, input: &str) -> usize {
-    println!("Hello, world!");
-    0
+pub fn get_garden_plots_in_steps(num_steps: u32, input: &str) -> usize {
+    let garden = read_garden(input);
+
+    let mut has_seen_path = HashSet::<(usize, usize, u32)>::new();
+    let mut has_seen_final_plot = HashSet::<(usize, usize)>::new();
+
+    let mut path_queue = LinkedList::<(usize, usize, u32)>::new();
+    path_queue.push_back((garden.start_x, garden.start_y, 0));
+
+    while !path_queue.is_empty() {
+        let (x, y, step_i) = path_queue.pop_front().unwrap();
+
+        match step_i.cmp(&num_steps) {
+            Ordering::Equal => {
+                has_seen_final_plot.insert((x, y));
+            }
+            Ordering::Less => {
+                [[x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]]
+                    .iter()
+                    .for_each(|[next_x, next_y]| {
+                        let next_cell = garden.get_cell(*next_x, *next_y);
+
+                        match next_cell {
+                            Some(Cell::Garden) | Some(Cell::Start) => {
+                                if !has_seen_path.contains(&(*next_x, *next_y, step_i + 1)) {
+                                    path_queue.push_back((*next_x, *next_y, step_i + 1));
+
+                                    has_seen_path.insert((*next_x, *next_y, step_i + 1));
+                                }
+                            }
+                            _ => {}
+                        }
+                    });
+            }
+            Ordering::Greater => {}
+        }
+    }
+
+    println!("{:?}", has_seen_final_plot);
+
+    has_seen_final_plot.len()
 }
 
-fn read_garden() -> Garden {
-    new_garden()
+fn read_garden(input: &str) -> Garden {
+    let mut start_x = 0;
+    let mut start_y = 0;
+    let mut width = 0;
+    let mut height = 0;
+
+    let inner_map = input.split('\n').enumerate().fold(
+        HashMap::<String, Cell>::new(),
+        |mut map, (row_i, row)| {
+            row.chars().enumerate().for_each(|(col_i, c)| {
+                let cell = Cell::from_char(c).unwrap();
+
+                if cell == Cell::Start {
+                    start_x = col_i;
+                    start_y = row_i;
+                }
+
+                map.insert(Garden::get_key(col_i, row_i), cell);
+
+                if col_i > width {
+                    width = col_i;
+                }
+            });
+
+            if row_i > height {
+                height = row_i;
+            }
+
+            map
+        },
+    );
+
+    new_garden(inner_map, start_x, start_y, width + 1, height + 1)
 }
 
 #[cfg(test)]
@@ -29,6 +103,6 @@ mod test {
 
     #[test]
     fn test_get_garden_plots_in_steps() {
-        assert_eq!(16, get_garden_plots_in_steps(6, TEST_INPUT))
+        assert_eq!(16, get_garden_plots_in_steps(6, TEST_INPUT));
     }
 }
