3cd41abb1182c9d3290b995403f0998ba230acce
John Webb <john.webb94@gmail.com>
2024-02-11 19:11:26 -0700

    Setup test

--- rust/year2023/day22/src/lib.rs
+++ rust/year2023/day22/src/lib.rs
@@ -1,6 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+pub fn get_safetly_disintegrated(input: &str) -> u32 {
+    0
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use crate::get_safetly_disintegrated;
+
+    pub const TEST_INPUT_1: &str = "1,0,1~1,2,1
+0,0,2~2,0,2
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9";
+
+    #[test]
+    fn test_get_safetly_disintegrated() {
+        assert_eq!(5, get_safetly_disintegrated(TEST_INPUT_1));
+    }
+}
