b2653dea12091180d3ed812840a9a6e329855ce3
John Webb <john.webb94@gmail.com>
2024-12-09 00:05:15 -0700

    Get part one

--- rust/year2024/day09/src/lib.rs
+++ rust/year2024/day09/src/lib.rs
@@ -1,22 +1,23 @@
-pub fn get_filesystem_checksum(disk_map_string: &str) -> u128 {
+pub fn get_filesystem_checksum(disk_map_string: &str) -> usize {
     let disk_map = disk_map_string
         .chars()
-        .map(|c| c.to_string().parse::<u8>().unwrap())
-        .collect::<Vec<u8>>();
+        .map(|c| c.to_string().parse::<_>().unwrap())
+        .collect::<Vec<usize>>();
 
+    // Note: This is really memory hungry
     let files = disk_map.iter().step_by(2).enumerate().collect::<Vec<_>>();
-    let spaces = disk_map.iter().skip(1).step_by(2).collect::<Vec<&u8>>();
+    let spaces = disk_map.iter().skip(1).step_by(2).collect::<Vec<&usize>>();
 
     let disk = files
         .iter()
         .enumerate()
         .fold(vec![], |mut v, (file_i, (file_id, file_length))| {
             for _ in 0..**file_length {
-                v.push(file_id.to_string().chars().next().unwrap());
+                v.push(Some(file_id));
             }
 
             for _ in 0..**spaces.get(file_i).unwrap_or(&&0) {
-                v.push('.');
+                v.push(None);
             }
 
             v
@@ -24,27 +25,25 @@ pub fn get_filesystem_checksum(disk_map_string: &str) -> u128 {
 
     let mut disk_i = 0;
     let mut sorted_disk_i = disk.len() - 1;
-    let mut sorted_disk: Vec<char> = vec![];
+    let mut sorted_disk: Vec<usize> = vec![];
 
     while disk_i <= sorted_disk_i {
-        if disk[disk_i] == '.' {
-            sorted_disk.push(disk[sorted_disk_i]);
+        if disk[disk_i] == None {
+            sorted_disk.push(*disk.get(sorted_disk_i).unwrap_or(&None).unwrap_or(&0));
 
             sorted_disk_i -= 1;
-            while disk[sorted_disk_i] == '.' {
+            while disk[sorted_disk_i] == None {
                 sorted_disk_i -= 1;
             }
         } else {
-            sorted_disk.push(disk[disk_i]);
+            sorted_disk.push(*disk.get(disk_i).unwrap_or(&None).unwrap_or(&0));
         }
 
         disk_i += 1;
     }
 
-    dbg!(&sorted_disk);
-
     sorted_disk.iter().enumerate().fold(0, |sum, (c_i, c)| {
-        sum + c_i as u128 * c.to_string().parse::<u128>().unwrap()
+        sum + c_i * c.to_string().parse::<usize>().unwrap()
     })
 }
 
@@ -62,10 +61,10 @@ mod tests {
         let file = fs::read_to_string("./input.txt").unwrap();
         let input = file.as_str();
 
-        // assert_eq!(get_filesystem_checksum(TEST_1), 0);
+        assert_eq!(get_filesystem_checksum(TEST_1), 60);
         assert_eq!(get_filesystem_checksum(TEST_2), 1928);
 
         // < 5909851146
-        assert_eq!(get_filesystem_checksum(input), 0);
+        assert_eq!(get_filesystem_checksum(input), 6386640365805);
     }
 }
