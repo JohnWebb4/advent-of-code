afa26e07c107b980c768838041f0b96d084c0c4c
John Webb <john.webb@smartrent.com>
2025-12-03 16:43:54 -0700

    Get part one

--- year2017/day22/src/lib.cpp
+++ year2017/day22/src/lib.cpp
@@ -1,49 +1,171 @@
 #include "lib.h"
 
+#include <expected>
 #include <format>
-#include <iostream>
+#include <memory>
 #include <string>
 #include <unordered_set>
 
 namespace year2017::day22
 {
   const std::string get_node_key(int x, int y);
+
+  enum class rotation
+  {
+    left = -1,
+    right = 1
+  };
+
+  enum class direction
+  {
+    up = 0,
+    right = 1,
+    down = 2,
+    left = 3,
+  };
+  const std::string direction_to_str(day22::direction direction);
+
+  class vector2
+  {
+  public:
+    int x;
+    int y;
+
+    vector2(int _x, int _y);
+  };
+
+  const std::expected<vector2, std::string> map_direction_to_vector(day22::direction direction);
+  direction rotate(direction direction, rotation rotation);
 } // year2017::day22
 
-int year2017::day22::count_bursts_causing_infection(const std::string_view &input_string)
+namespace year2017::day22
 {
-  std::unordered_set<std::string> node_set{};
 
-  int x = 0;
-  int y = 0;
-  for (std::string_view::iterator hi = input_string.begin(); input_string.end(); hi++)
+  int count_bursts_causing_infection(const std::string_view &input_string)
   {
-    if (*hi == '.')
+    size_t size_x = input_string.find('\n');
+    size_t size_y = input_string.size() / (size_x + 1);
+
+    std::unordered_set<std::string> node_set{};
+
+    int x = -(size_x / 2);
+    int y = -(size_y / 2);
+
+    for (size_t i = 0; i < input_string.size(); i++)
     {
-      x++;
+      if (input_string[i] == '.')
+      {
+        x++;
+      }
+      else if (input_string[i] == '#')
+      {
+        node_set.insert(day22::get_node_key(x, y));
+        x++;
+      }
+      else if (input_string[i] == '\n')
+      {
+        x = -(size_x / 2);
+        y++;
+      }
     }
-    else if (*hi == '#')
+
+    int virus_x = 0;
+    int virus_y = 0;
+    day22::direction virus_direction = day22::direction::up;
+
+    int count_infections = 0;
+    const int max_bursts = 10000;
+    for (int i = 0; i < max_bursts; i++)
     {
-      node_set.insert(day22::get_node_key(x, y));
-      x++;
+      std::string currenty_key = day22::get_node_key(virus_x, virus_y);
+
+      if (node_set.contains(currenty_key))
+      {
+        virus_direction = day22::rotate(virus_direction, day22::rotation::right);
+        node_set.erase(currenty_key);
+      }
+      else
+      {
+        virus_direction = day22::rotate(virus_direction, day22::rotation::left);
+        node_set.insert(currenty_key);
+        count_infections++;
+      }
+
+      // std::cout << "Move " << day22::direction_to_str(virus_direction) << " from " << virus_x << "," << virus_y << std::endl;
+
+      std::unique_ptr<day22::vector2> position_diff = std::make_unique<day22::vector2>(day22::map_direction_to_vector(virus_direction).value());
+
+      virus_x = virus_x + position_diff->x;
+      virus_y = virus_y + position_diff->y;
     }
-    else if (*hi == '\n')
-    {
-      x = 0;
-      y++;
-    }
+
+    // Implementation goes here
+    return count_infections;
   }
 
-  for (auto hi : node_set)
+  const std::expected<day22::vector2, std::string> map_direction_to_vector(day22::direction direction)
   {
-    std::cout << hi << std::endl;
+    if (direction == day22::direction::up)
+    {
+      return day22::vector2(0, -1);
+    }
+    else if (direction == day22::direction::right)
+    {
+      return day22::vector2(1, 0);
+    }
+    else if (direction == day22::direction::down)
+    {
+      return day22::vector2(0, 1);
+    }
+    else if (direction == day22::direction::left)
+    {
+      return day22::vector2(-1, 0);
+    }
+
+    return std::unexpected("Cannot map direction to value");
   }
 
-  // Implementation goes here
-  return 0;
-}
+  const std::string direction_to_str(day22::direction direction)
+  {
+    if (direction == day22::direction::up)
+    {
+      return "UP";
+    }
+    else if (direction == day22::direction::right)
+    {
+      return "RIGHT";
+    }
+    else if (direction == day22::direction::down)
+    {
+      return "DOWN";
+    }
+    else if (direction == day22::direction::left)
+    {
+      return "LEFT";
+    }
 
-const std::string year2017::day22::get_node_key(int x, int y)
-{
-  return std::format("{0},{1}", x, y);
-}
+    return "UNKNOWN";
+  }
+
+  day22::vector2::vector2(int x, int y) : x(x), y(y)
+  {
+  }
+
+  direction rotate(direction direction, rotation rotation)
+  {
+    int next_direction_value = static_cast<int>(direction) + static_cast<int>(rotation);
+
+    if (next_direction_value < 0)
+    {
+      next_direction_value += 4;
+    }
+
+    return static_cast<day22::direction>(next_direction_value % 4);
+  }
+
+  const std::string get_node_key(int x, int y)
+  {
+    return std::format("{0},{1}", x, y);
+  }
+
+} // year2017::day22
--- year2017/day22/test/lib_test.cpp
+++ year2017/day22/test/lib_test.cpp
@@ -2,6 +2,9 @@
 
 #include <gtest/gtest.h>
 
+#include <fstream>
+#include <sstream>
+
 TEST(Year2017, Day22)
 {
     const std::string test_1 = R"(..#
@@ -9,5 +12,11 @@ TEST(Year2017, Day22)
 ...
 )";
 
+    const std::ifstream input_file{"../input.txt"};
+    std::stringstream input_stream{};
+
+    input_stream << input_file.rdbuf();
+
     EXPECT_EQ(year2017::day22::count_bursts_causing_infection(test_1), 5587);
+    EXPECT_EQ(year2017::day22::count_bursts_causing_infection(input_stream.str()), 5406);
 }
