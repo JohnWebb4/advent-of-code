7d725c74299f5ddfe7f56cc75f6a543eccd7118d
John Webb <john.webb@smartrent.com>
2025-12-12 16:24:36 -0700

    Stash work

--- year2025/day10/src/lib.cpp
+++ year2025/day10/src/lib.cpp
@@ -1,6 +1,144 @@
 #include "lib.h"
 
+#include <charconv>
+#include <string_view>
+#include <vector>
+
+#include <iostream>
 namespace year2025::day10
 {
+  enum class ButtonState
+  {
+    on,
+    off
+  };
+
+  class WiringSchematic
+  {
+  public:
+    std::vector<int> wiring;
+
+    WiringSchematic(const std::vector<int> &wiring) : wiring(wiring) {}
+  };
+  class Machine
+  {
+  public:
+    std::vector<ButtonState> light_diaghram;
+    std::vector<WiringSchematic> wiring_schematics;
+    std::vector<int> voltages;
+
+    Machine(const std::vector<bool> &light_diaghram, const std::vector<WiringSchematic> &wiring_schematics, const std::vector<int> &voltages) : light_diaghram(light_diaghram), wiring_schematics(wiring_schematics), voltages(voltages) {}
+  };
+
+  void parse_light_diaghram(std::vector<ButtonState> &light_diaghram, std::string_view light_diaghram_view)
+  {
+    for (std::size_t char_index = 1; char_index < light_diaghram_view.size() - 1; char_index++)
+    {
+      switch (light_diaghram_view.at(char_index))
+      {
+      case '.':
+        light_diaghram.emplace_back(false);
+        break;
+      case '#':
+        light_diaghram.emplace_back(true);
+        break;
+      default:
+        throw "Error parsing light diaghram";
+      }
+    }
+  }
+
+  WiringSchematic parse_wiring_schematic(std::string_view wiring_schematic_view)
+  {
+    std::vector<int> buttons{};
+    std::size_t prev_comma_index = 0;
+    for (std::size_t comma_index = wiring_schematic_view.find(','); comma_index != -1; comma_index = wiring_schematic_view.find(',', prev_comma_index))
+    {
+      std::string_view value_view = wiring_schematic_view.substr(prev_comma_index, comma_index - prev_comma_index);
+
+      int value{};
+      if (auto [p, ec] = std::from_chars(value_view.begin(), value_view.end(), value); ec == std::errc{})
+      {
+        buttons.emplace_back(value);
+      }
+      prev_comma_index = comma_index + 1;
+    }
+
+    std::cout << "HI " << wiring_schematic_view << std::endl;
+
+    return WiringSchematic(buttons);
+  }
+
+  void parse_wiring_schematics(std::vector<WiringSchematic> &wiring_schematics, std::string_view wiring_schematics_view)
+  {
+    std::size_t prev_space_index = 0;
+    for (std::size_t space_index = wiring_schematics_view.find(' '); space_index != -1; space_index = wiring_schematics_view.find(' ', prev_space_index))
+    {
+      wiring_schematics.emplace_back(parse_wiring_schematic(wiring_schematics_view.substr(prev_space_index, space_index - prev_space_index)));
+
+      prev_space_index = space_index + 1;
+    }
+    wiring_schematics.emplace_back(parse_wiring_schematic(wiring_schematics_view.substr(prev_space_index)));
+    int hi = 0;
+  }
+
+  void parse_voltages(std::vector<int> &voltages, std::string_view voltages_view)
+  {
+  }
+
+  Machine parse_machine(std::string_view machine_instruction)
+  {
+
+    std::size_t light_diaghram_index = machine_instruction.find(']') + 1;
+
+    std::string_view light_diaghram_view = machine_instruction.substr(
+        0,
+        light_diaghram_index);
+    std::vector<bool> light_diaghram{};
+    parse_light_diaghram(light_diaghram, light_diaghram_view);
+
+    std::size_t wiring_schematics_index = machine_instruction.find('{');
+    std::string_view wiring_schematics_view = machine_instruction.substr(
+        light_diaghram_index + 1,
+        wiring_schematics_index - light_diaghram_index - 2);
+    std::vector<WiringSchematic> wiring_schematics{};
+    parse_wiring_schematics(wiring_schematics, wiring_schematics_view);
+
+    std::string_view voltages_view = machine_instruction.substr(
+        wiring_schematics_index);
+    std::vector<int> voltages{};
+    parse_voltages(voltages, voltages_view);
+
+    return Machine(light_diaghram, wiring_schematics, voltages);
+  }
+
+  void parse_machines(std::vector<Machine> &machines, std::string_view manual_instructions)
+  {
+    std::size_t prev_eol_index = 0;
+    std::size_t eol_index = 0;
+    for (std::size_t eol_index = manual_instructions.find('\n'); eol_index != -1; eol_index = manual_instructions.find('\n', eol_index + 1))
+    {
+      std::string_view machine_instruction = manual_instructions.substr(prev_eol_index, eol_index - prev_eol_index);
+
+      machines.emplace_back(parse_machine(machine_instruction));
+
+      prev_eol_index = eol_index + 1;
+    }
+
+    {
+      std::string_view machine_instruction = manual_instructions.substr(prev_eol_index);
+
+      machines.emplace_back(parse_machine(machine_instruction));
+    }
+  }
+
+  long long count_fewest_presses_to_configure(std::string_view manual_instructions)
+  {
+    std::vector<Machine> machines;
+
+    parse_machines(machines, manual_instructions);
+
+    return 0ll;
+  }
 
 } // year2025::day10
--- year2025/day10/test/lib_test.cpp
+++ year2025/day10/test/lib_test.cpp
@@ -2,7 +2,13 @@
 
 #include <gtest/gtest.h>
 
+#include <string>
+
 TEST(Year2025, Day10)
 {
-    EXPECT_EQ(0, 0);
+    const std::string test_1 = R"([.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5})";
+
+    EXPECT_EQ(year2025::day10::count_fewest_presses_to_configure(test_1), 0);
 }
