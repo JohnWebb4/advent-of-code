b911f3b6e63019f705114ea1d4a0134376ec8055
John Webb <John.Webb94@gmail.com>
2025-12-13 18:39:39 -0700

    Saving work

--- year2025/day10/src/lib.cpp
+++ year2025/day10/src/lib.cpp
@@ -1,10 +1,13 @@
 #include "lib.h"
 
 #include <charconv>
+#include <climits>
+#include <queue>
 #include <string_view>
 #include <vector>
 
 #include <iostream>
+
 namespace year2025::day10
 {
   enum class ButtonState
@@ -20,6 +23,7 @@ namespace year2025::day10
 
     WiringSchematic(const std::vector<int> &wiring) : wiring(wiring) {}
   };
+
   class Machine
   {
   public:
@@ -28,6 +32,15 @@ namespace year2025::day10
     std::vector<int> voltages;
 
     Machine(const std::vector<ButtonState> &light_diaghram, const std::vector<WiringSchematic> &wiring_schematics, const std::vector<int> &voltages) : light_diaghram(light_diaghram), wiring_schematics(wiring_schematics), voltages(voltages) {}
+  };
+
+  class MachineSolution
+  {
+  public:
+    std::vector<ButtonState> light_diaghram;
+    std::vector<std::size_t> button_press_path;
+
+    MachineSolution(const std::vector<ButtonState> &light_diaghram, const std::vector<std::size_t> &button_press_path) : light_diaghram(light_diaghram), button_press_path(button_press_path) {}
   };
 
   void parse_light_diaghram(std::vector<ButtonState> &light_diaghram, std::string_view light_diaghram_view)
@@ -64,10 +77,10 @@ namespace year2025::day10
       prev_comma_index = comma_index + 1;
     }
     std::string_view final_value_view = wiring_schematic_view.substr(prev_comma_index, wiring_schematic_view.size() - prev_comma_index - 1);
-    int final_value{ 0 };
+    int final_value{0};
     if (auto [p, ec] = std::from_chars(final_value_view.data(), final_value_view.data() + final_value_view.size(), final_value); ec == std::errc())
     {
-        buttons.emplace_back(final_value);
+      buttons.emplace_back(final_value);
     }
 
     return WiringSchematic(buttons);
@@ -87,13 +100,26 @@ namespace year2025::day10
 
   void parse_voltages(std::vector<int> &voltages, std::string_view voltages_view)
   {
-      std::size_t prev_comma_index = 1;
-      int value{0};
-      for (auto [p, ec] = std::from_chars(voltages_view.data(), voltages_view.data() + voltages_view.size(), value); ec == std::errc())
-      {
+    std::size_t prev_comma_index = 1;
+    for (std::size_t comma_index = voltages_view.find(','); comma_index != -1; comma_index = voltages_view.find(',', prev_comma_index))
+    {
+      std::string_view voltage_view = voltages_view.substr(prev_comma_index, comma_index - prev_comma_index);
 
+      int value{0};
+      if (auto [p, ec] = std::from_chars(voltage_view.data(), voltage_view.data() + voltage_view.size(), value); ec == std::errc())
+      {
+        voltages.emplace_back(value);
       }
 
+      prev_comma_index = comma_index + 1;
+    }
+
+    std::string_view final_value_view = voltages_view.substr(prev_comma_index, voltages_view.size() - prev_comma_index - 1);
+    int final_value{0};
+    if (auto [p, ec] = std::from_chars(final_value_view.data(), final_value_view.data() + final_value_view.size(), final_value); ec == std::errc())
+    {
+      voltages.emplace_back(final_value);
+    }
   }
 
   Machine parse_machine(std::string_view machine_instruction)
@@ -142,13 +168,114 @@ namespace year2025::day10
     }
   }
 
+  long long count_fewest_presses_to_configure_machine(const Machine &machine)
+  {
+    auto get_value = [&machine](const MachineSolution &solution)
+    {
+      int cost = solution.button_press_path.size();
+      int value = 0;
+      for (std::size_t button_i = 0; button_i < solution.light_diaghram.size(); button_i++)
+      {
+        if (machine.light_diaghram[button_i] == solution.light_diaghram[button_i])
+        {
+          value += 5;
+        }
+      }
+      return value - cost;
+    };
+
+    auto comp = [&machine, &get_value](const MachineSolution &l, const MachineSolution &r)
+    {
+      int l_value = get_value(l);
+      int r_value = get_value(r);
+
+      return l_value < r_value;
+    };
+
+    // Find solution
+    std::priority_queue<MachineSolution, std::vector<MachineSolution>, decltype(comp)>
+        solution_queue{comp};
+
+    std::vector<ButtonState> initial_light_diaghram{};
+    for (int i = 0; i < machine.light_diaghram.size(); i++)
+    {
+      initial_light_diaghram.emplace_back(ButtonState::off);
+    }
+    solution_queue.emplace(MachineSolution(initial_light_diaghram, std::vector<std::size_t>{}));
+
+    long long num_fewest_presses{LLONG_MAX};
+    while (solution_queue.size() > 0)
+    {
+      const MachineSolution solution = solution_queue.top();
+      solution_queue.pop();
+
+      if (solution.button_press_path.size() < num_fewest_presses)
+      {
+        bool has_solved = true;
+        for (std::size_t button_i = 0; button_i < solution.light_diaghram.size(); button_i++)
+        {
+          if (solution.light_diaghram[button_i] != machine.light_diaghram[button_i])
+          {
+            has_solved = false;
+            break;
+          }
+        }
+
+        // Check match
+        if (has_solved)
+        {
+          std::cout << "Solved ";
+          for (auto &button : solution.button_press_path)
+          {
+            std::cout << button << " ";
+          }
+          std::cout << std::endl;
+
+          long long num_button_presses = solution.button_press_path.size() + 1;
+          if (num_button_presses < num_fewest_presses)
+          {
+            num_fewest_presses = num_button_presses;
+          }
+          break;
+        }
+        else if (solution.button_press_path.size() < num_fewest_presses - 1)
+        {
+          for (std::size_t wiring_schematic_i = 0; wiring_schematic_i < machine.wiring_schematics.size(); wiring_schematic_i++)
+          {
+            std::vector<ButtonState> next_light_diaghram = solution.light_diaghram;
+            for (int button : machine.wiring_schematics.at(wiring_schematic_i).wiring)
+            {
+              next_light_diaghram[button] = next_light_diaghram[button] == ButtonState::on ? ButtonState::off : ButtonState::on;
+            }
+
+            std::vector<std::size_t> next_button_path = solution.button_press_path;
+            next_button_path.emplace_back(wiring_schematic_i);
+
+            solution_queue.emplace(MachineSolution(next_light_diaghram, next_button_path));
+          }
+        }
+      }
+    }
+
+    return num_fewest_presses;
+  }
+
   long long count_fewest_presses_to_configure(std::string_view manual_instructions)
   {
     std::vector<Machine> machines;
 
     parse_machines(machines, manual_instructions);
 
-    return 0ll;
+    long long num_fewest_presses_to_configure{0};
+    std::size_t num_machines{0};
+    for (Machine &machine : machines)
+    {
+      std::cout << "Looking at machine " << num_machines << std::endl;
+      num_fewest_presses_to_configure += count_fewest_presses_to_configure_machine(machine);
+      num_machines++;
+    }
+
+    return num_fewest_presses_to_configure;
   }
 
 } // year2025::day10
