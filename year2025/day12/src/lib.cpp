e2c2871a3942f3f5313579da7f898da92257e0e8
John Webb <John.Webb94@gmail.com>
2025-12-27 00:00:04 -0700

    Working on part one

--- year2025/day12/src/lib.cpp
+++ year2025/day12/src/lib.cpp
@@ -39,8 +39,18 @@ namespace year2025::day12
             }
         }
 
-        Shape(const std::unordered_map<std::string, ShapeValues> &values, std::size_t width, std::size_t height) : values(values), width(width), height(height)
+        static const std::string get_key(std::size_t x, std::size_t y)
         {
+            return std::format("{},{}", x, y);
+        }
+
+        Shape(const std::unordered_map<std::string, ShapeValues> &values, std::size_t width, std::size_t height, long long surface_area) : values(values), width(width), height(height)
+        {
+        }
+
+        const ShapeValues &at(std::size_t x, std::size_t y) const
+        {
+            return this->values.at(Shape::get_key(x, y));
         }
     };
 
@@ -63,7 +73,108 @@ namespace year2025::day12
         SituationSummary(const std::vector<Shape> &present_shapes, const std::vector<Region> &regions) : present_shapes(present_shapes), regions(regions) {}
     };
 
-    std::unique_ptr<SituationSummary> parse_situation_summary(const std::string_view situtation_summary_view)
+    Shape parse_shape(const std::string_view shape_view)
+    {
+        std::unordered_map<std::string, ShapeValues> values;
+        std::size_t width{0};
+
+        std::size_t prev_line_index = 0;
+        std::size_t line_i = 0;
+        long long surface_area = 0;
+        while (prev_line_index != -1)
+        {
+            std::size_t line_index = shape_view.find('\n', prev_line_index);
+            std::string_view line = shape_view.substr(prev_line_index, line_index - prev_line_index);
+
+            // Skip first line
+            if (line_i != 0)
+            {
+                for (std::size_t char_i = 0; char_i < line.size(); char_i++)
+                {
+                    if (std::optional<ShapeValues> value = Shape::parse_shape_values(line[char_i]); value)
+                    {
+                        if (*value == ShapeValues::SOLID)
+                        {
+                            values.emplace(Shape::get_key(char_i, line_i - 1), *value);
+                        }
+                    }
+                }
+                width = std::max(width, line.size());
+            }
+
+            if (line_index == -1)
+            {
+                break;
+            }
+            else
+            {
+                prev_line_index = line_index + 1;
+                line_i++;
+            }
+        }
+
+        return Shape(values, width, line_i, surface_area);
+    }
+
+    const static std::string REGION_PART_DELIM = ": ";
+    const static std::string DIMENSIONS_PART_DELIM = "x";
+    Region parse_region(const std::string_view region_view)
+    {
+        std::size_t part_delim_index = region_view.find(REGION_PART_DELIM);
+        std::string_view dimensions_view = region_view.substr(0, part_delim_index);
+        std::size_t dimension_delim_index = dimensions_view.find(DIMENSIONS_PART_DELIM);
+        std::size_t width = std::atoi(static_cast<std::string>(dimensions_view.substr(0, dimension_delim_index)).c_str());
+        std::size_t height = std::atoi(static_cast<std::string>(dimensions_view.substr(dimension_delim_index + DIMENSIONS_PART_DELIM.size())).c_str());
+
+        std::vector<long long> num_presents;
+        std::string_view num_presents_view = region_view.substr(part_delim_index + REGION_PART_DELIM.size());
+        std::size_t prev_present_index = 0;
+        while (prev_present_index != -1)
+        {
+            std::size_t present_index = num_presents_view.find(' ', prev_present_index);
+            std::string_view present_view = num_presents_view.substr(prev_present_index, present_index - prev_present_index);
+
+            num_presents.emplace_back(std::atoi(static_cast<std::string>(present_view).c_str()));
+
+            if (present_index == -1)
+            {
+                break;
+            }
+            else
+            {
+                prev_present_index = present_index + 1;
+            }
+        }
+
+        return Region(num_presents, width, height);
+    }
+
+    std::vector<Region> parse_regions(const std::string_view regions_view)
+    {
+        std::vector<Region> regions;
+
+        std::size_t prev_line_index = 0;
+        while (prev_line_index != -1)
+        {
+            std::size_t line_index = regions_view.find('\n', prev_line_index);
+            std::string_view region_view = regions_view.substr(prev_line_index, line_index - prev_line_index);
+
+            regions.emplace_back(parse_region(region_view));
+
+            if (line_index == -1)
+            {
+                break;
+            }
+            else
+            {
+                prev_line_index = line_index + 1;
+            }
+        }
+
+        return regions;
+    }
+
+    SituationSummary parse_situation_summary(const std::string_view situtation_summary_view)
     {
         std::vector<Shape> present_shapes{};
         std::vector<Region> regions{};
@@ -74,25 +185,48 @@ namespace year2025::day12
             std::size_t section_index = situtation_summary_view.find("\n\n", prev_section_index);
             std::string_view section_view = situtation_summary_view.substr(prev_section_index, section_index - prev_section_index);
 
-            int hi = 0;
-
             if (section_index == -1)
             {
+                regions = parse_regions(section_view);
                 break;
             }
             else
             {
+                present_shapes.emplace_back(parse_shape(section_view));
+
                 prev_section_index = section_index + 2;
             }
         }
 
-        return std::make_unique<SituationSummary>(SituationSummary(present_shapes, regions));
+        return SituationSummary(present_shapes, regions);
     }
 
     long long count_regions_can_fit_presents(std::string_view input_view)
     {
-        std::unique_ptr<SituationSummary> situation_summary = parse_situation_summary(input_view);
+        std::unique_ptr<SituationSummary> situation_summary = std::make_unique<SituationSummary>(parse_situation_summary(input_view));
 
-        return 0;
+        long long num_regions_can_fit{0};
+
+        for (const Region &region : situation_summary->regions)
+        {
+            // Simple area test. If tests take up more area than avaialable then stop.
+            long long region_area = region.width * region.height;
+
+            long long total_present_area{0};
+            for (std::size_t num_present_i = 0; num_present_i < region.num_presents.size(); num_present_i++)
+            {
+                const Shape &present_shape = situation_summary->present_shapes.at(num_present_i);
+
+                total_present_area += region.num_presents.at(num_present_i) * situation_summary->present_shapes.at(num_present_i).values.size();
+            }
+
+            if (total_present_area < region_area)
+            {
+                // TODO: Do more checks
+                num_regions_can_fit++;
+            }
+        }
+
+        return num_regions_can_fit;
     }
 } // year2025::day12
--- year2025/day12/test/lib_test.cpp
+++ year2025/day12/test/lib_test.cpp
@@ -2,8 +2,10 @@
 
 #include <gtest/gtest.h>
 
+#include <fstream>
 #include <string>
 #include <string_view>
+#include <sstream>
 
 TEST(Year2025, Day12)
 {
@@ -41,5 +43,12 @@ TEST(Year2025, Day12)
 12x5: 1 0 1 0 2 2
 12x5: 1 0 1 0 3 2)";
 
+    std::ifstream file_stream{"../input.txt"};
+    std::stringstream input_stream{};
+    input_stream << file_stream.rdbuf();
+
     EXPECT_EQ(year2025::day12::count_regions_can_fit_presents(static_cast<std::string_view>(TEST_CASE_1)), 2);
+    EXPECT_EQ(year2025::day12::count_regions_can_fit_presents(static_cast<std::string_view>(input_stream.view())), 2);
+
+    file_stream.close();
 }
