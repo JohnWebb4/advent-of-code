2870f8ffeac62d15117b168de21a370696b59e0e
John Webb <John.Webb94@gmail.com>
2026-08-30 13:50:15 -0700

    Stash work

--- year2018/day04/src/libguard.c
+++ year2018/day04/src/libguard.c
@@ -13,12 +13,8 @@ struct libguard_sleep_times
 
 int libguard_get_strategy_1_best_guard(struct libguard_event *events, size_t events_length)
 {
-    int num_sleep_times = 0;
+    size_t num_sleep_times = 0;
     struct libguard_sleep_times sleep_times[10];
-
-    int guard_ids[10];
-    int guard_minutes_asleep[10];
-    size_t num_guards = 0;
 
     int current_guard = -1;
     struct tm *fell_asleep_time = NULL;
@@ -29,23 +25,6 @@ int libguard_get_strategy_1_best_guard(struct libguard_event *events, size_t eve
         if (event->event_type == BEGIN_SHIFT)
         {
             current_guard = event->guard_id;
-
-            bool already_exists = false;
-            for (size_t guard_i = 0; guard_i < num_guards; guard_i++)
-            {
-                if (guard_ids[guard_i] == event->guard_id)
-                {
-                    already_exists = true;
-                    break;
-                }
-            }
-
-            if (!already_exists)
-            {
-                guard_ids[num_guards] = event->guard_id;
-                guard_minutes_asleep[num_guards] = 0;
-                num_guards++;
-            }
         }
         else if (event->event_type == FALLS_ASLEEP)
         {
@@ -70,23 +49,38 @@ int libguard_get_strategy_1_best_guard(struct libguard_event *events, size_t eve
 
             int diff_minutes = minute_awake - minute_asleep;
 
-            bool guard_exists = false;
-            for (size_t guard_i = 0; guard_i < num_guards; guard_i++)
-            {
-                if (guard_ids[guard_i] == current_guard)
-                {
-                    guard_minutes_asleep[guard_i] += diff_minutes;
-                    guard_exists = true;
-                }
-            }
-
-            if (!guard_exists)
-            {
-                perror("Guard wakes up but never existed");
-                return -1;
-            }
+            sleep_times[num_sleep_times].guard_id = current_guard;
+            sleep_times[num_sleep_times].minutes_asleep = diff_minutes;
+            num_sleep_times++;
 
             fell_asleep_time = NULL;
+        }
+    }
+
+    int guard_ids[10];
+    int guard_minutes_asleep[10];
+    size_t num_guards = 0;
+
+    for (size_t sleep_time_i = 0; sleep_time_i < num_sleep_times; sleep_time_i++)
+    {
+        const struct libguard_sleep_times *sleep_time = &sleep_times[sleep_time_i];
+
+        bool alread_exists = false;
+        for (size_t guard_i = 0; guard_i < num_guards; guard_i++)
+        {
+            if (guard_ids[guard_i] == sleep_time->guard_id)
+            {
+                guard_minutes_asleep[guard_i] += sleep_time->minutes_asleep;
+                alread_exists = true;
+                break;
+            }
+        }
+
+        if (!alread_exists)
+        {
+            guard_ids[num_guards] = sleep_time->guard_id;
+            guard_minutes_asleep[num_guards] = sleep_time->minutes_asleep;
+            num_guards++;
         }
     }
 
